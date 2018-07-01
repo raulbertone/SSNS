19e0f298244e7e14631dcb318d9b8f90b7c55336
Raul Bertone <raul.bertone@emptyingthebuffer.com>
2018-07-01 12:00:19 +0200

    Improved Fall Notification Service. Now it should send only one email for fall event Also, after help has been requested, the user can still signal a false alarm.

--- source/desktop_application/src/main/java/com/application/util/FallNotificationService.java
+++ source/desktop_application/src/main/java/com/application/util/FallNotificationService.java
@@ -6,7 +6,7 @@ import com.application.mainController;
  * This class handles the notifications triggered by the detection of a fall.
  * On start it idles while waiting for a trigger. When the fall detection
  * algorithm thinks the user has fallen, it alerts the FNS which turns on and off
- * the buzzer on the Sensortags and the flags in the GUI accordingly.
+ * the buzzer on the Sensortags, the flags in the GUI and sends emails accordingly.
  *
  * @author Raul Bertone
  */
@@ -17,10 +17,10 @@ public class FallNotificationService implements Runnable{
 	private boolean run = true;
 	private boolean falseAlarm = false;
 	private mainController controller;
+	private boolean fallHappened = false;
 
 	static{
 		instance = new FallNotificationService();
-
 		(new Thread(instance)).start();
 
     }
@@ -40,8 +40,8 @@ public class FallNotificationService implements Runnable{
 	}
 
 	// call this method to signal a false alarm (when the user presses a button on the Sensortag)
-	public static void falseAlarm() {
-		instance.falseAlarm = true;
+	public static void notifyFalseAlarm() {
+		instance.setFalseAlarm();
 	}
 
 	public static void setMain(mainController controller) {
@@ -59,6 +59,11 @@ public class FallNotificationService implements Runnable{
 		}
 	}
 
+	private synchronized void setFalseAlarm() {
+		instance.falseAlarm = true;
+		notify();
+	}
+
 	private synchronized void waitForFall() {
 
 		// wait on this
@@ -70,6 +75,17 @@ public class FallNotificationService implements Runnable{
 			}
 		}
 
+		if(falseAlarm & fallHappened) {
+			// TODO turn off the buzzer
+			instance.controller.setLblFallDetColor("#000000"); // lower Fall flag in the GUI
+			instance.controller.setLblHelpReqColor("#000000"); // lower Help flag in the GUI
+
+			String message = " says it was a false alarm, everything's fine";
+			sendMail(message);
+			fallHappened = false;
+			return;
+		}
+
 		// to prevent random button presses to incorrectly "pre-declare" a fall as a false alarm
 		falseAlarm = false;
 
@@ -78,7 +94,7 @@ public class FallNotificationService implements Runnable{
 
 		// wait to see if the user signals a false alarm
 		try {
-			wait(ConfigurationStorage.getHELP_REQUEST_DELAY());
+			Thread.sleep(ConfigurationStorage.getHELP_REQUEST_DELAY());
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
@@ -86,9 +102,13 @@ public class FallNotificationService implements Runnable{
 		if (falseAlarm) {
 			// TODO turn off the buzzer
 			instance.controller.setLblFallDetColor("#000000"); // lower the flag in the GUI
+			falseAlarm = false;
 		} else {
+			fallHappened = true;
 
-			requestHelp(); // send email
+			String message = " has fallen and requires assistance";
+
+			sendMail(message); // send email
 			instance.controller.setLblHelpReqColor("#a20000"); // raise flag in the GUI
 
 			fallDetected = false;
@@ -101,9 +121,8 @@ public class FallNotificationService implements Runnable{
 	}
 
 	// helper method that prepares the email and sends it
-	private void requestHelp() {
-		String messageBody = ConfigurationStorage.getFIRST_NAME() + " " + ConfigurationStorage.getLAST_NAME() + " living at " + ConfigurationStorage.getADDRESS() +
-							 " has fallen and requires assistance";
+	private void sendMail(String message) {
+		String messageBody = ConfigurationStorage.getFIRST_NAME() + " " + ConfigurationStorage.getLAST_NAME() + " living at " + ConfigurationStorage.getADDRESS() + message;
 		String toAddress = ConfigurationStorage.getCONTACT_PERSON_EMAIL();
 
 		try {
